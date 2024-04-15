3623ab5ab7d7b2df0a97afd737dad92783dda637
damcodegym <146150052+damcodegym@users.noreply.github.com>
2024-04-15 15:57:49 +0700

    oop2

--- java/src/bai3.java
+++ java/src/bai3.java
@@ -2,14 +2,13 @@ public class bai3 {
     public static void main(String[] args) {
         System.out.println("Các số nguyên tố nhỏ hơn 100 là:");
         for (int number = 2; number < 100; number++) {
-            if (isPrime(number)) {
+            if (fist(number)) {
                 System.out.print(number + " ");
             }
         }
     }
 
-    // Phương thức kiểm tra xem một số có phải là số nguyên tố hay không
-    public static boolean isPrime(int num) {
+    public static boolean fist(int num) {
         if (num <= 1) {
             return false;
         }
